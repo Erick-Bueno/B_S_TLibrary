8f7541b21635c56585dbcfe44a68086fde1044c1
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-10-28 15:36:59 -0300

    Library

---
+++ B_S_TLibrary/BinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿namespace B_S_TLibrary;
+//tree structure
+
+public class BinarySearchTree
+{
+    public int NodeValue { get; set; }
+    //binary trees have nodes with at most two children
+    private BinarySearchTree leftNode { get; set; }
+    private BinarySearchTree rightNode { get; set; }
+
+    public BinarySearchTree(int NodeValue)
+    {
+        this.NodeValue = NodeValue;
+        leftNode = null;
+        rightNode = null;
+    }
+    public void Insert(int value)
+    {
+        if (value < NodeValue)
+        {
+            if (leftNode == null)
+            {
+                leftNode = new BinarySearchTree(value);
+                return;
+            }
+            leftNode.Insert(value);
+            return;
+        }
+
+        if (rightNode == null)
+        {
+            rightNode = new BinarySearchTree(value);
+            return;
+        }
+
+        rightNode.Insert(value);
+        return;
+
+
+    }
+    public BinarySearchTree find(int value)
+    {
+        if (value == NodeValue)
+        {
+            return this;
+        }
+        if (value < NodeValue)
+        {
+            if (leftNode != null)
+            {
+                return leftNode.find(value);
+            }
+            return null;
+            
+        }
+        if(rightNode != null){
+            return rightNode.find(value);
+        }
+        return null;
+    }
+
+ 
+}
